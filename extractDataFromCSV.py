39060fc28c4ff716aaec30df42486bba2ea6b0d5
smilemax <smilemax@foxmail.com>
2021-02-07 11:43:57 +0800

    first

--- extractDataFromCSV.py
+++ extractDataFromCSV.py
@@ -1,9 +1,24 @@
-import pandas as pd     # 读写表格
-import numpy as np      # 向量操作
-import yaml             # 配置文件
-import meshio           # 读写网格
+"""
+Copyright 2021 Chen Xiaoxiao
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 import multiprocessing  # 多线程
+
+import meshio  # 读写网格
+import numpy as np  # 向量操作
+import pandas as pd  # 读写表格
+import yaml  # 配置文件
 
 
 class pressureMeasurement:
